133be7e3320d57bdd5688909850d5bdc3e03af3b
AlanF <maildoalana@gmail.com>
2021-04-08 21:28:13 +0200

    api upgrade

--- consumer.py
+++ consumer.py
@@ -1,4 +1,7 @@
+import json
 import pika
+
+from main import Product, db
 
 params = pika.URLParameters('amqps://zobyksdf:tEXN4vKXrQskmY9Lmi39zqk5IcdHzkSE@kangaroo.rmq.cloudamqp.com/zobyksdf')
 
@@ -11,10 +14,31 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, propreties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if propreties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif propreties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif propreties.content_type == 'product_deleted':
+        product = Product.query.get(data['id'])
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
 
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
